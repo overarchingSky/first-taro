67f8b032134df4b4a07a9e0cb790a80648bf63b1
tianlong <380989353@qq.com>
2019-09-18 18:46:13 +0800

    添加测试代码，并集成travis

---
+++ test/url.test.ts
@@ -0,0 +1,7 @@
+import { Url } from '../src/utils/url'
+
+test('test:utils/url.js',() => {
+    expect(Url.join('a','b')).toBe('a/b')
+    expect(Url.join('a/','b')).toBe('a/b')
+    expect(Url.join('a','/b')).toBe('ab')
+})
