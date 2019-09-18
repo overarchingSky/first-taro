338ab222321053f7962150445a3eb055c6a234a4
tianlong <380989353@qq.com>
2019-09-18 18:51:44 +0800

    修正测试文件

--- test/url.test.ts
+++ test/url.test.ts
@@ -3,5 +3,5 @@ import { Url } from '../src/utils/url'
 test('test:utils/url.js',() => {
     expect(Url.join('a','b')).toBe('a/b')
     expect(Url.join('a/','b')).toBe('a/b')
-    expect(Url.join('a','/b')).toBe('ab')
+    expect(Url.join('a','/b')).toBe('a/b')
 })
