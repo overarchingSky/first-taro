f1433890e8a97465d7ebe76fab4d92679a05f35e
tianlong <380989353@qq.com>
2019-09-21 17:31:20 +0800

    feat: ww
    ww

--- script/prepare-commit-msg.js
+++ script/prepare-commit-msg.js
@@ -1,4 +1,10 @@
 const shelljs = require('shelljs')
+if(process.env.allowGitCommit){
+    
+}else{
+    shelljs.exec(`yarn commit`)
+    return 'exit 1'
+}
 if(!process.env.noVerifyHook){
     shelljs.exec(`exec < /dev/tty &&  git cz --hook || true`)
 }
