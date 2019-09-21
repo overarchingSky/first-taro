123f8c68fad69cfd7cd2449bfd04b73b4c8d5a9e
tianlong <380989353@qq.com>
2019-09-21 15:55:06 +0800

    test: 测试ci流程
    测试ci流程详情

--- script/commit.js
+++ script/commit.js
@@ -1,5 +1,4 @@
 const shelljs = require('shelljs')
-console.log('verifyHook:',process.env.verifyHook)
 if(!process.env.noVerifyHook){
     shelljs.exec(`exec < /dev/tty &&  git cz --hook || true`)
 }
--- script/commit.sh
+++
@@ -1,3 +0,0 @@
-exec < /dev/tty &&  git cz --hook || true
-
-"prepare-commit-msg": "sudo sh ./script/commit.sh"
