ae8ba113456659149338b2ab40880ed25a26ff1a
tianlong <380989353@qq.com>
2019-09-27 17:36:56 +0800

    ci: ci优化
    添加提交时的自动lint以及修复功能

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,12 +1,16 @@
 type action = {
-    type:string,
-    payload:any
+  type: string;
+  payload: any;
+};
+let a = [1, 2, 3, 4, 5, 6];
+console.log("a", a);
+export function createAction(type: string) {
+  return (payload: any): action => {
+    return {
+      type,
+      payload
+    };
+  };
 }
-export function createAction(type:string) {
-    return (payload:any):action => {
-        return {
-            type,
-            payload
-        }
-    }
-}
+
+export default createAction;
